26e965202c809e9fb57425f2ba43b5bf7f6c6b68
joshuaolarte <joshuaolarte02@hotmail.com>
2021-09-11 18:49:11 -0500

    algoritmo casi completo logrando hallar tambien los bisiestos y agregarlos

--- cumplea_os_final/cumplea_os_final.ino
+++ cumplea_os_final/cumplea_os_final.ino
@@ -1,6 +1,6 @@
  int dia = 10;
   int mes = 12;
-  int year = 2005;
+  int year = 2018;
   int dia_actual = 11;
   int mes_actual = 9;
   int year_actual = 2021;
@@ -8,7 +8,7 @@
   String diaSem[] = {"lunes", "martes", "miercoles", "jueves" , "viernes", "sabado", "domingo"} ;
   int dias_enero = 31, dias_febrero = 28, dias_marzo = 31, dias_abril = 30, dias_mayo = 31, dias_junio = 30, dias_julio = 31, dias_agosto = 31, dias_septiembre = 30, dias_octubre = 31, dias_noviembre = 30, dias_diciembre = 31;
   int dia_resta;
-  int diferencia;
+  int dias_acum;
   int contador;
   boolean flag = 0;
   String dia_hoy = "sabado";
@@ -28,10 +28,28 @@ int identificar_mes (int x){
 
 void setup() {
   Serial.begin(9600);
+ for(int contador_years = 1; contador_years <= (year_actual - year); contador_years ++ ){
+  Serial.println(contador_years);
+  if(contador_years % 4){
+    dias_acum ++;
 
-  
+  } 
+  if (contador_years == 1){
+    Serial.println("voy a contar meses");
+    for(int contador_meses = mes_actual ; contador_meses >= 1 ; contador_meses --){
+      if(contador_meses == mes_actual){
+       dias_acum = dia_actual;
+
+      }else{
+        dias_acum = dias_acum + identificar_mes(contador_meses);
+      }
+
+    }
+  }
+ }
+  Serial.println(dias_acum);
 }
-
+/*
   void algoritmoFinal(){
      diferencia = dia;
    for(contador=0; contador < diferencia; contador ++){
@@ -46,6 +64,7 @@ void setup() {
    
    }
   }
+ */ 
 void loop() {
 }
      
